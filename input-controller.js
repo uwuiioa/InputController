75cbab4e26643e41801fd9a7decbaaa32b6938c1
uwuiioa <kristya.shalnova@mail.ru>
2025-08-22 13:43:44 +0300

    Update input-controller.js

--- input-controller.js
+++ input-controller.js
@@ -1,148 +1,130 @@
-const InputController = (function() {
-    class InputController {
-        static ACTION_ACTIVATED = "input-controller:action-activated";
-        static ACTION_DEACTIVATED = "input-controller:action-deactivated";
-
-        constructor(actionsToBind = {}, target = document) {
-            this._enabled = true;
-            this.focused = true;
-            this.target = target;
-
-            this.actions = {};
-            this.keyPressed = {};
-
-            this.handleKeyDown = this.handleKeyDown.bind(this);
-            this.handleKeyUp = this.handleKeyUp.bind(this);
-
-            this.bindActions(actionsToBind);
-            this.attach(target);
-        }
-
-        set enabled(value) {
-            if (this._enabled !== value) {
-                this._enabled = value;
-                if (!value) {
-                    this.resetAllActions();
-                }
-            }
-        }
-
-        get enabled() {
-            return this._enabled;
-        }
-
-        bindActions(actionsToBind) {
-            for (const [actionName, parameters] of Object.entries(actionsToBind)) {
-                this.actions[actionName] = {
-                    keys: Array.isArray(parameters.keys) ? parameters.keys : [parameters.keys],
-                    active: false,
-                    enabled: parameters.enabled !== undefined ? parameters.enabled : true
-                }
-                for (const key of this.actions[actionName].keys) {
-                    if (!this.keyPressed[key]) {
-                        this.keyPressed[key] = new Set();
-                    }
-                    this.keyPressed[key].add(actionName);
-                }
-            }
-        }
-
-        enableAction(actionName) {
-            if (this.actions[actionName]) {
-                this.actions[actionName].enabled = true;               
-            }
-        }
-
-        disableAction(actionName) {
-            if (this.actions[actionName]) {
-                this.actions[actionName].enabled = false;  
-            }
-        }
-
-        attach(target, dontEnable = false) {
-            this.target = target;
-            target.addEventListener('keydown', this.handleKeyDown);
-            target.addEventListener('keyup', this.handleKeyUp);
-            if (!dontEnable) {
-                this.enabled = true;   
-            }
-        }
-
-        detach() {
-            if (this.target) {
-                this.target.removeEventListener('keydown', this.handleKeyDown);
-                this.target.removeEventListener('keyup', this.handleKeyUp);
-            }
-            this.enabled = false;
-            this.resetAllActions();
-        }
-
-        resetAllActions() {
-            for (const actionName in this.actions) {
-                if (this.actions[actionName].active) {
-                    this.actions[actionName].active = false;
-                    const event = new CustomEvent(InputController.ACTION_DEACTIVATED, {
-                        detail: { action: actionName }
-                    });
-                    window.dispatchEvent(event);
-                }
-            }
-        }
-
-        isActionActive(action) {
-            if (this.actions[action]) {
-                return this.actions[action].active && this.actions[action].enabled;
-            }
-            return false;
-        }
-
-        isKeyPressed(keyCode) {
-            const actions = this.keyPressed[keyCode];
-            if (actions) {
-                for (const actionName of actions) {
-                    if (this.actions[actionName].enabled && this.actions[actionName].active) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        handleKeyDown(e) {
-            if (!this.enabled || !this.focused) return;
-            const keyCode = e.keyCode;
-            const actions = this.keyPressed[keyCode];
-            if (actions) {
-                for (const actionName of actions) {
-                    const action = this.actions[actionName];
-                    if (action.enabled && !action.active) {
-                        action.active = true;
-                        const event = new CustomEvent(InputController.ACTION_ACTIVATED, {
-                            detail: { action: actionName }
-                        });
-                        window.dispatchEvent(event);
-                    }
-                }
-            }
-        }
+class InputController {
+    constructor() {
+        this.plugins = new Map();
+        this.actions = new Map();
+        this.enabled = true;
+        this.focused = true;
+        this.target = null;
         
-        handleKeyUp(e) {
-            if (!this.enabled || !this.focused) return;
-            const keyCode = e.keyCode;
-            const actions = this.keyPressed[keyCode];
-            if (actions) {
-                for (const actionName of actions) {
-                    const action = this.actions[actionName];
-                    if (action.enabled && action.active) {
-                        action.active = false;
-                        const event = new CustomEvent(InputController.ACTION_DEACTIVATED, {
-                            detail: { action: actionName }
-                        });
-                        window.dispatchEvent(event);
-                    }
-                }
-            }
+        this.ACTION_ACTIVATED = 'input-controller:action-activated';
+        this.ACTION_DEACTIVATED = 'input-controller:action-deactivated';
+        
+        this.handleFocus = this.handleFocus.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+    }
+
+    registerAction(actionName) {
+        if (!this.actions.has(actionName)) {
+            this.actions.set(actionName, {
+                active: false,
+                plugins: new Set()
+            });
         }
     }
-    return InputController;
-})();
+
+    setActionState(actionName, isActive) {
+        if (!this.actions.has(actionName)) {
+            this.registerAction(actionName);
+        }
+        
+        const action = this.actions.get(actionName);
+        
+        if (action.active !== isActive) {
+            action.active = isActive;
+            
+            const eventName = isActive ? this.ACTION_ACTIVATED : this.ACTION_DEACTIVATED;
+            const event = new CustomEvent(eventName, {
+                detail: { action: actionName }
+            });
+            
+            if (this.target) {
+                this.target.dispatchEvent(event);
+            }
+            
+            console.log(`Action '${actionName}' ${isActive ? 'activated' : 'deactivated'}`);
+        }
+    }
+
+    isActionActive(actionName) {
+        const action = this.actions.get(actionName);
+        return action ? action.active : false;
+    }
+
+    addPlugin(name, plugin) {
+        if (this.plugins.has(name)) {
+            console.warn(`Plugin '${name}' already exists`);
+            return;
+        }
+        
+        this.plugins.set(name, plugin);
+        plugin.init(this);
+        console.log(`Plugin '${name}' added`);
+        
+        if (this.target) {
+            plugin.attach(this.target);
+        }
+    }
+
+    removePlugin(name) {
+        const plugin = this.plugins.get(name);
+        if (plugin) {
+            plugin.detach();
+            plugin.destroy();
+            this.plugins.delete(name);
+            console.log(`Plugin '${name}' removed`);
+        }
+    }
+
+    attach(target) {
+        this.target = target;
+        
+        target.addEventListener('click', this.handleFocus);
+        target.addEventListener('focus', this.handleFocus);
+        target.addEventListener('blur', this.handleBlur);
+        
+        this.plugins.forEach(plugin => {
+            plugin.attach(target);
+        });
+        
+        console.log('Controller attached to target');
+    }
+
+    detach() {
+        if (this.target) {
+            this.target.removeEventListener('click', this.handleFocus);
+            this.target.removeEventListener('focus', this.handleFocus);
+            this.target.removeEventListener('blur', this.handleBlur);
+            
+            this.plugins.forEach(plugin => {
+                plugin.detach();
+            });
+            
+            this.target = null;
+            console.log('Controller detached from target');
+        }
+    }
+
+    handleFocus() {
+        this.focused = true;
+        console.log('Controller focused');
+    }
+
+    handleBlur() {
+        this.focused = false;
+        console.log('Controller blurred');
+        
+        this.actions.forEach((action, actionName) => {
+            if (action.active) {
+                this.setActionState(actionName, false);
+            }
+        });
+    }
+
+    destroy() {
+        this.detach();
+        this.plugins.forEach(plugin => plugin.destroy());
+        this.plugins.clear();
+        this.actions.clear();
+        console.log('Controller destroyed');
+    }
+}
